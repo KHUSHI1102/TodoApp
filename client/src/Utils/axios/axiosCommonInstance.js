d64d5e4e66c27ed3f9799c9886290269373f64bc
Khushi Tala <khushi02alphabitinfoway@gmail.com>
2025-08-12 11:29:36 +0530

    Update axiosCommonInstance.js

--- client/src/Utils/axios/axiosCommonInstance.js
+++ client/src/Utils/axios/axiosCommonInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: 'http://65.2.38.186/api/',
   headers: {
     'Content-Type': 'application/json',
   },
